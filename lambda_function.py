d3164e31bb3941568d3e98532a4f91fa3cf34232
christinandrea <christiputri5@gmail.com>
2022-10-30 18:55:27 +0700

    add inputFile

--- lambda_function.py
+++ lambda_function.py
@@ -8,13 +8,11 @@ import PIL.Image
             
 s3_client = boto3.client('s3')
             
-def open_image(image_path):
-    Image.open(image_path) 
-  # as image:
-    # image.thumbnail(tuple(x / 2 for x in image.size))
-    # image.save(resized_path)
-     
-
+def resize_image(image_path, resized_path):
+  with Image.open(image_path) as image:
+    image.thumbnail(tuple(x / 2 for x in image.size))
+    image.save(resized_path)
+            
 def lambda_handler(event, context):
   for record in event['Records']:
     bucket = record['s3']['bucket']['name']
@@ -23,6 +21,5 @@ def lambda_handler(event, context):
     download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
     upload_path = '/tmp/resized-{}'.format(tmpkey)
     s3_client.download_file(bucket, key, download_path)
-    open_image(download_path)
-    # resize_image(download_path, upload_path)
-    # s3_client.upload_file(upload_path, '{}-resized'.format(bucket), 'resized-{}'.format(key))
+    resize_image(download_path, upload_path)
+    s3_client.upload_file(upload_path, '{}-resized'.format(bucket), 'resized-{}'.format(key))
